9a3cdf42c71d6b75048dbc2aa03c183feb957ff5
agraharamkavya <agrakavya1997@gmail.com>
2026-03-25 14:01:49 +0530

    protected frontend routes

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -3,10 +3,12 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
 
+  // ❌ If not logged in → redirect to login
   if (!token) {
     return <Navigate to="/" />;
   }
 
+  // ✅ If logged in → allow access
   return children;
 }
 
