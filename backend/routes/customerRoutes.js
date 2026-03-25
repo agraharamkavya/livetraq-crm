8af4e787a1a5c5fdda5332bd6f5e0bc77bf38563
agraharamkavya <agrakavya1997@gmail.com>
2026-03-25 11:15:08 +0530

    protected routes with auth middleware

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -3,18 +3,27 @@ const express = require("express");
 const router = express.Router();
 const Customer = require("../models/Customer");
 
-
 // 👉 Add Customer
-router.post("/add",authMiddleware, async (req, res) => {
-  const customer = new Customer(req.body);
-  await customer.save();
-  res.json(customer);
+router.post("/add", authMiddleware, async (req, res) => {
+  try {
+    const customer = new Customer(req.body);
+    await customer.save();
+    res.json(customer);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error adding customer" });
+  }
 });
 
 // 👉 Get All Customers
-router.get("/",authMiddleware, async (req, res) => {
-  const customers = await Customer.find();
-  res.json(customers);
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    res.json(customers);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error fetching customers" });
+  }
 });
 
 module.exports = router;
