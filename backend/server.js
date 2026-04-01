4ab3eb591a49ed34fcab1f8c110867df241253bc
agraharamkavya <agrakavya1997@gmail.com>
2026-04-01 13:22:43 +0530

    fix react routing refresh

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,13 @@ mongoose.connect(process.env.MONGO_URI)
 app.get("/", (req, res) => {
   res.send("CRM Backend Running 🚀");
 });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
