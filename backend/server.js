9518989b2ead24bee83895bd45f0f7af9924cf8c
agraharamkavya <agrakavya1997@gmail.com>
2026-04-03 11:32:32 +0530

    fix express wildcard route

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("/_redirects*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
